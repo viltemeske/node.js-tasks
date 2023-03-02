f173a162a05fff073b51b88032517c287000bdb9
viltemeske <meshkenai@gmail.com>
2023-03-02 14:46:57 +0200

    created replace animal mutation

--- src/animals/index.ts
+++ src/animals/index.ts
@@ -4,18 +4,15 @@ import getAnimal from './queries/get-animal';
 import getAnimals from './queries/get-animals';
 import createAnimal from './mutations/create-animal';
 import deleteAnimal from './mutations/delete-animal';
+import replaceAnimal from './mutations/replace-animal';
 
 const animalsRouter = express.Router();
 
 animalsRouter.get('/', getAnimals);
 animalsRouter.get('/:id', getAnimal);
-
 animalsRouter.post('/', createAnimal);
-
-animalsRouter.patch('/:id', (req, res) => {
-  res.send('edit one animal');
-});
-
+animalsRouter.put('/:id', replaceAnimal);
+// animalsRouter.patch('/:id', updateAnimal);
 animalsRouter.delete('/:id', deleteAnimal);
 
 export default animalsRouter;
