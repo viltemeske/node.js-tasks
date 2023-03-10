02b6b3ef63a149e0352ad7c1e88bcd9ecff0acab
viltemeske <meshkenai@gmail.com>
2023-03-10 13:00:01 +0200

    data insertion joisn example

---
+++ sql/examples/data insertion joins.sql
@@ -0,0 +1,30 @@
+select
+	a.animalId as id,
+	a.name, 
+    json_object(
+		'id', u.userId,
+        'name', u.name,
+        'surname', u.surname,
+        'mobile', u.mobile,
+        'email', u.email
+	) as fosterer,
+ json_object(
+		'rūšis', sp.title,
+	'tipas', st.title
+	) as tipas,
+-- count(a.animalId)
+-- sum(a.userId)
+-- a.animalId, count(i.src)
+a.animalId, json_arrayagg(i.src) as images
+from animal as a 
+join user u
+on a.userId = u.userId
+join animalspecies as sp
+on a.animalSpeciesId = sp.animalSpeciesId
+join speciestype as st
+on sp.speciesTypeId = st.speciesTypeId
+join animalimage as ai
+on a.animalId = ai.animalId
+join image as i
+on ai.imageId = i.imageId
+group by a.animalId
